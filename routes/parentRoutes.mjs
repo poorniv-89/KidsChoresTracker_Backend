8abe264f32a44fba50f4c64f84819ed8fc2b2049
poorniv-89 <poorniv.89@gmail.com>
2025-06-03 10:00:45 -0600

    Post route to add chores to existing parent added

--- routes/parentRoutes.mjs
+++ routes/parentRoutes.mjs
@@ -3,13 +3,14 @@ import Parent from '../models/parentsSchema.mjs';
 
 const router = express.Router();
 
+//Adding new parent
 router.post('/', async (req, res, next) => {
-    
+
     const existingParent = await Parent.findOne({ email: req.body.email });
     if (existingParent) {
         const error = new Error('You are already a registered user!');
         error.status = 400;
-        return next(error); 
+        return next(error);
     }
 
     const newParent = new Parent(req.body);
@@ -24,5 +25,42 @@ router.post('/', async (req, res, next) => {
         }
     });
 });
+
+//Adding new chores to existing parent 
+router.post('/:parentId/chores', async (req, res, next) => {
+    try{
+    const { parentId } = req.params;
+    const newChores = req.body;
+
+    const choresToAdd = Array.isArray(newChores) ? newChores : [newChores];
+    const parent = await Parent.findById(parentId);
+    if (!parent) {
+        const error = new Error('Parent not found!');
+        error.status = 404;
+        return next(error);
+    }
+
+    if (!choresToAdd.length) {
+        const error = new Error('Please provide at least one chore!');
+        error.status = 400;
+        return next(error);
+        
+    }
+
+    parent.chores.push(...choresToAdd);
+    await parent.save();
+
+    res.status(200).json({
+        message: 'Chores added successfully',
+        chores: parent.chores
+      });
+    }
+    catch(err)
+    {
+        next(err);
+    }
+})
+
+
 
 export default router;
